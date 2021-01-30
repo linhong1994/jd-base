d7824f8fb4ecfab420989a7d329bf5e10fa61135
linhong1994 <330362495@qq.com>
2021-01-30 11:13:58 +0800

    1
    1

--- git_pull.sh
+++ git_pull.sh
@@ -31,13 +31,9 @@ isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
 WhichDep=$(grep "/jd-base" "${ShellDir}/.git/config")
 Scripts2URL=https://github.com/shylocks/Loon
 
-if [[ ${WhichDep} == *github* ]]; then
-  ScriptsURL=https://github.com/LXK9301/jd_scripts
-  ShellURL=https://github.com/EvineDeng/jd-base
-else
-  ScriptsURL=https://gitee.com/lxk0301/jd_scripts
-  ShellURL=https://gitee.com/evine/jd-base
-fi
+
+ScriptsURL=https://gitee.com/lxk0301/jd_scripts
+ShellURL=https://github.com/linhong1994/jd-base
 
 ## 更新shell脚本
 function Git_PullShell {
